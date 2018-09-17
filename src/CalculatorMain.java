e4bc6a392b95f54227d78534c9bdbc909ec3901c
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-09-17 18:43:50 -0400

    Add files via upload

--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -5,8 +5,12 @@ public class CalculatorMain {
 	{
 		// This is how we create an object
 		// New --> java keyword which creates an object
+		
 		Calculator calc = new Calculator();
-		System.out.print(calc.add(2, 6));
+		System.out.println("The sum of 6 and 2 is " + calc.add(2, 6));
+		
+		CalculatorSubtract calculator = new CalculatorSubtract();
+		System.out.print("The difference between 37582 and 18574 is " + calculator.sub(37582, 18574));
 		
 		
 	}
---
+++ src/CalculatorSubtract.java
@@ -0,0 +1,13 @@
+
+public class CalculatorSubtract {
+
+	public int sub (int num1, int num2) 
+	{
+		int ans = num1 - num2;
+		return ans;
+		
+		
+	}
+	
+	
+}
